bed668f67b872555ee9d0b003449fc788a12347e
cmolinaord <carlosmolina.ord@gmail.com>
2018-06-11 03:51:42 +0200

    Correcting code. Now actually measures contrast in corners

--- src/analize.py
+++ src/analize.py
@@ -1,27 +1,39 @@
+import sys
 import numpy as np
-import cv2
+import cv2 as cv
 
-cropY = 0.08
-cropX = 0.16
+args = sys.argv
+filename = args[1]
 
-photo = cv2.imread('test/image.jpg')
-height = photo.shape[0]
-width = photo.shape[1]
+photo = cv.imread(filename)
+h = photo.shape[0]
+w = photo.shape[1]
 
-corner1 = photo[0:round(cropY * height),	0:round(cropX * width)]
-corner2 = photo[-round(cropY * height):-1,	0:round(cropX * width)]
-corner3 = photo[0:round(cropY * height), 	-round(cropX * width):-1]
-corner4 = photo[-round(cropY * height):-1, 	-round(cropX * width):-1]
+roi_w = round(0.16 * w)
+roi_h = round(0.08 * h)
 
-# Mean by color
-corner1.mean(axis=(0,1))
+corner 			= np.zeros((4,roi_h,roi_w,3))
+corner[0,:,:,:] = photo[0:roi_h,	0:roi_w,:]
+corner[1,:,:,:] = photo[-roi_h-1:-1,	0:roi_w,:]
+corner[2,:,:,:] = photo[0:roi_h, 	-roi_w-1:-1,:]
+corner[3,:,:,:] = photo[-roi_h-1:-1, 	-roi_w-1:-1,:]
 
+# Corner: image in BGR
 
-cv2.imshow("corner1",corner1)
-cv2.imshow("corner2",corner2)
-cv2.imshow("corner3",corner3)
-cv2.imshow("corner4",corner4)
+for k in range(0,4):
+	# Convert to gray BGR -> Gray:  Y = 0.299R + 0.587G + 0.114B
+	b,g,r = cv.split(corner[k,:,:,:])
+	gray 	 	= 0.299*r + 0.587*g + 0.114*b
+	lum_mean 	= np.mean(gray)
+	lum_diff 	= np.max(gray) - np.min(gray)
+	lum_std 	= np.std(gray)
+	print("Corner ",k)
+	print("  mean = ",lum_mean)
+	print("  diff = ",lum_diff)
+	print("  std = ",lum_std)
+	print("  error = ",lum_std/lum_mean*100,"%")
+	cv.imshow("Corner "+str(k),gray/256)
 
-cv2.waitKey(10000)
-
-# dst = cv.cvtColor(photo,COLOR_BGR2HSV)
+key = cv.waitKey(0)
+if key == 13:
+	cv.destroyAllWindows()
