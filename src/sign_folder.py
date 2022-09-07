8714b69e9c65056e1350f0d6401bc625e7564c3f
cmolinaord <carlosmolina.ord@gmail.com>
2022-09-08 01:31:35 +0200

    Added functionality to keep EXIF data from original photo

--- src/sign_folder.py
+++ src/sign_folder.py
@@ -4,6 +4,7 @@ import numpy as np
 import cv2 as cv
 import tools as tool
 import time as t
+from PIL import	Image
 
 args = sys.argv
 folder_path = args[1]
@@ -17,10 +18,18 @@ print("Processing", num_photos, "photos in", folder_path)
 print("Using signature in", sign_path)
 
 for filename in tool.get_files(folder_path):
+	# Extract EXIF from original image
+	im = Image.open(filename)
+	exif_data = im.info['exif']
+
+	# Run Signature tool
 	signed = tool.sign_image(filename,sign_path,verbose=True)
 	signed_filename = dst_path + "/" + filename
 	cv.imwrite(signed_filename,signed)
-	#cv.imshow(filename,signed)
+
+	# Write EXIF on new image
+	im_new = Image.open(signed_filename)
+	image_new.save(signed_filename, 'JPEG', exif=exif_data)
 
 t2 = t.time() - t1
 print(" ")
